664d73a416d201727c122568fa879c5f28b5e714
hp-yYp <1092916593@qq.com>
2020-05-04 23:01:47 +0800

    后台中间作业

---
+++ src/main/java/com/example2/demo/entity/RoleInfo.java
@@ -0,0 +1,116 @@
+package com.example2.demo.entity;
+
+import java.util.Date;
+
+/**
+ * tgfh
+ *
+ * @Author: wwb
+ * @Date: 2020/5/4 21:20
+ */
+public class RoleInfo {
+    private String id;
+
+    private String roleName;
+
+    private Integer isDeleted;
+
+    private String sortNo;
+
+    private Date gmtCreate;
+
+    private String createBy;
+
+    private Date gmtModified;
+
+    private String lastModifiedBy;
+
+    private Integer version;
+
+    @Override
+    public String toString() {
+        return "RoleInfo{" +
+                "id='" + id + '\'' +
+                ", roleName='" + roleName + '\'' +
+                ", isDeleted=" + isDeleted +
+                ", sortNo='" + sortNo + '\'' +
+                ", gmtCreate=" + gmtCreate +
+                ", createBy='" + createBy + '\'' +
+                ", gmtModified=" + gmtModified +
+                ", lastModifiedBy='" + lastModifiedBy + '\'' +
+                ", version=" + version +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public String getSortNo() {
+        return sortNo;
+    }
+
+    public void setSortNo(String sortNo) {
+        this.sortNo = sortNo;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+}
